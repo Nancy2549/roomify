51593590648118d6c391b4a99ce2bae763cec86e
unknown <nancychauhan418@gmail.com>
2026-06-25 21:22:02 +0530

    host and upload images

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -14,7 +14,7 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const { isSignedIn } = useOutletContext<AuthContext>();
+
 
   return (
     <div className="home">
--- app/routes/visualize.&id.tsx
+++ app/routes/visualize.&id.tsx
@@ -1,10 +1,7 @@
-import type { Route } from "../+types/root";
+import { useLocation } from "react-router";
 
-export default function Visualize({ params }: Route.ComponentProps) {
-  return (
-    <div>
-      <h1>Visualizer {"params.id"}</h1>
-    </div>
-  );
+const VisualizerId = () => {
+      const
+
 }
 
